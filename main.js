c6f3cdb13204b07f5c02e2866b9e33c0521596fe
Mark Bryan <marklawbryan@gmail.com>
2022-12-15 13:16:16 -0800

    slight styling changes

--- main.js
+++ main.js
@@ -6,10 +6,9 @@
 //sad cat
 // https://cdn-icons-png.flaticon.com/512/12/12430.png
 
-const dogSynonyms = ["canine", "doggo", "puppericino", "pooch", "pup", "tail-wagger", "good ol' boi"];
+const dogSynonyms = ["doggo", "puppericino", "pooch", "tail-wagger", "good ol' boi"];
 
 const background = document.querySelector("._31N0dvxfpsO6Ur5AKx4O5d");
-console.log(document);
 background.style.backgroundImage =
   "url(https://thumbs.dreamstime.com/b/dog-paw-seamless-pattern-vector-cat-puppy-footprint-wallpaper-background-tile-125665632.jpg)";
 
@@ -49,7 +48,7 @@ document.addEventListener(`scroll`, function (e) {
 
   const allText = document.querySelectorAll("._eYtD2XCVieq6emjKBH3m, ._1qeIAgB0cPwnLhDF9XSiJM");
   allText.forEach(el => {
-    el.innerHTML = el.innerHTML.replace("dog", `<span style="color: yellow; text-shadow: 0px 0px 2px blue; font-family:GoodBoy;">${dogSynonyms[Math.floor(Math.random() * dogSynonyms.length)]}</span>`);
+    el.innerHTML = el.innerHTML.replace("dog", `<span style="opacity: 0.5; color: blue; text-shadow: 0px 0px 2px yellow; font-family:GoodBoy;">${dogSynonyms[Math.floor(Math.random() * dogSynonyms.length)]}</span>`);
     // el.className = "goodBoy";
   })
 });
