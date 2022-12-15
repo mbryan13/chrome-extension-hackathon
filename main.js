4887ba7b7bcd6757efd43e843e5d24cba6a73fa8
Mark Bryan <marklawbryan@gmail.com>
2022-12-14 22:44:22 -0800

    replaced instances of dog with synonyms. added new font and stylesheet

--- main.js
+++ main.js
@@ -6,7 +6,10 @@
 //sad cat
 // https://cdn-icons-png.flaticon.com/512/12/12430.png
 
+const dogSynonyms = ["canine", "doggo", "puppericino", "pooch", "pup", "tail-wagger", "good ol' boi"];
+
 const background = document.querySelector("._31N0dvxfpsO6Ur5AKx4O5d");
+console.log(document);
 background.style.backgroundImage =
   "url(https://thumbs.dreamstime.com/b/dog-paw-seamless-pattern-vector-cat-puppy-footprint-wallpaper-background-tile-125665632.jpg)";
 
@@ -22,6 +25,12 @@ window.addEventListener("load", function (e) {
     ele.style.backgroundImage =
       "url(https://cdn-icons-png.flaticon.com/512/12/12430.png)";
   });
+
+  const allText = document.querySelectorAll("._eYtD2XCVieq6emjKBH3m, ._1qeIAgB0cPwnLhDF9XSiJM");
+  allText.forEach(el => {
+    el.innerHTML = el.innerHTML.replace("dog", dogSynonyms[Math.floor(Math.random() * dogSynonyms.length)]);
+  })
+
 });
 
 document.addEventListener(`scroll`, function (e) {
@@ -37,6 +46,12 @@ document.addEventListener(`scroll`, function (e) {
     ele.style.backgroundImage =
       "url(https://cdn-icons-png.flaticon.com/512/12/12430.png)";
   });
+
+  const allText = document.querySelectorAll("._eYtD2XCVieq6emjKBH3m, ._1qeIAgB0cPwnLhDF9XSiJM");
+  allText.forEach(el => {
+    el.innerHTML = el.innerHTML.replace("dog", `<span style="color: yellow; text-shadow: 0px 0px 2px blue; font-family:GoodBoy;">${dogSynonyms[Math.floor(Math.random() * dogSynonyms.length)]}</span>`);
+    // el.className = "goodBoy";
+  })
 });
 
 //PAWPRINT BACKGROUND SECTION
